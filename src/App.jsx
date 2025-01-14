4494f0e73b1595a0b39d6617d02cab4f50e747a5
BatiBati <94386708+BatiBati@users.noreply.github.com>
2025-01-14 18:30:06 +0800

    Weather app started

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import { useState } from "react";
+import { countriesData } from "./data";
+import LeftLogo from "./icons/Pinecone-Logo-Left";
+import RightLogo from "./icons/Pinecone-Logo-Right";
 
 function App() {
   return (
     <>
-      <div className="flex h-full relative">
+      <div className="relative flex justify-center items-center h-full ">
+        <div className="bg-white w-6/12 h-screen relative"></div>
+        <div className="bg-stone-900 text-white w-6/12 h-screen relative"></div>
+        <div className="absolute flex justify-center items-center">
+          <div className="absolute flex justify-center items-center gap-[20px] bg-white p-3 border rounded-full">
+            <LeftLogo />
+            <RightLogo />
+          </div>
+          <div className="absolute flex flex-col gap-28 justify-center items-center border-solid border rounded-full w-[300px] h-[300px]">
+            
+            <div className=" rounded-bl-full bg-[red] w-[60px] h-[60px]"></div>
+            <div className=" rounded-tl-full bg-[red] w-[60px] h-[60px]"></div>
 
-
-        <div className="flex justify-center border-solid outline-white absolute bottom 50% left 50% bg-orange-30 gap-4 flex justify-center">
-          <img src="vector.png" alt="" />
-          <img src="vector.png" alt="" className="rotate-180"/>
+          </div>
+          <div className="absolute flex justify-center items-center border-solid border rounded-full w-[600px] h-[600px]"></div>
+          <div className="absolute flex justify-center items-center border-solid border rounded-full w-[900px] h-[900px]"></div>
+          <div className="absolute flex justify-center items-center border-solid border rounded-full w-[1200px] h-[1200px]"></div>
+          <div className="absolute flex justify-center items-center border-solid border rounded-full w-[1500px] h-[1500px] overflow-y-hidden"></div>
+          <div className="absolute flex justify-center items-center border-solid border rounded-full w-[1800px] h-[1800px]"></div>
+          <div className="absolute flex justify-center items-center border-solid border rounded-full w-[2100px] h-[2100px]"></div>
         </div>
-        
-        
-
-        <div className="bg-slate-200 w-6/12 h-screen">Left</div>
-
-        <div className="bg-stone-900 text-white w-6/12 h-screen">Right</div>
       </div>
     </>
   );
